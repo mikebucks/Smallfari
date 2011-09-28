2ea00d7b405bf479c1a954d9c99c157c39c22590
Mike Bucks <dangerbucks@gmail.com>
2011-09-28 10:10:32 -0600

    fixes busted listener and reformats object literal in script.js

--- v1.safariextension/js/script.js
+++ v1.safariextension/js/script.js
@@ -1,54 +1,54 @@
-var SML = SML || {};
+var smallfari = {
 
-	var SML.m = {
+  deviceWidth: 320,
+  deviceHeight: 480,
+  rootNode: document.documentElement,
+  structure: document.body.childNodes,
 
-		deviceWidth: 320,
-		deviceHeight: 480,
-		rootNode: document.documentElement,
-		structure: document.body.childNodes,
+  handleMessage: function(msgEvent) {
+    var messageName = msgEvent.name;
+    if (messageName === 'mobilize') {
+      smallfari.resize();
+      smallfari.addViewport();
+    }
+  },
 
-		handleMessage: function(msgEvent) {
-			var messageName = msgEvent.name;
-			if (messageName === 'mobilize') {
-				SML.m.resize();
-				SML.m.addViewport();
-			}
-		},
+  resize: function() {
+    smallfari.rootNode.style.width = smallfari.deviceWidth + 'px';
+    smallfari.rootNode.style.margin = '0 auto';
+  },
 
-		resize: function() {
-			SML.m.rootNode.style.width = SML.m.deviceWidth + 'px';
-			SML.m.rootNode.style.margin = '0 auto';
-		},
+  addViewport: function() {  
+    var matte = document.createElement('div');
+    matte.setAttribute('id', 'mblzr_viewport');
+    matte.style.width = smallfari.deviceWidth + 'px';
+    matte.style.top = smallfari.deviceHeight + 'px';
+    matte.style.marginLeft = '-' + (smallfari.deviceWidth / 2) + 'px';
 
-		addViewport: function() {	
-			var matte = document.createElement('div');
-			matte.setAttribute('id', 'mblzr_viewport');
-			matte.style.width = SML.m.deviceWidth + 'px';
-			matte.style.top = SML.m.deviceHeight + 'px';
-			matte.style.marginLeft = '-' + (SML.m.deviceWidth / 2) + 'px';
+    document.getElementsByTagName('body')[0].appendChild(matte);
+  }
+}
 
-			document.getElementsByTagName('body')[0].appendChild(matte);
-		}
-	}
-
+//Listen for messages from global.html
+safari.self.addEventListener('message', smallfari.handleMessage, true);
 
 // TODO 
 // Insert menu items
 /*
 function addControls() {
-	var controlsHTML = '';
-	controlsHTML += '<li><a id="orientation" onClick="reorient();" href="#">Orientation</a></li>';
-	
-	var controls = document.createElement('ul');
-	controls.setAttribute('id', 'mblzr_controls');
-	
-	document.getElementsByTagName('body')[0].appendChild(controls);
-	controls.innerHTML = controlsHTML;
-	
-	var viewToggle = document.getElementById('orientation');
+  var controlsHTML = '';
+  controlsHTML += '<li><a id="orientation" onClick="reorient();" href="#">Orientation</a></li>';
+  
+  var controls = document.createElement('ul');
+  controls.setAttribute('id', 'mblzr_controls');
+  
+  document.getElementsByTagName('body')[0].appendChild(controls);
+  controls.innerHTML = controlsHTML;
+  
+  var viewToggle = document.getElementById('orientation');
 }
 
 function reorient() {
-	alert(this);
+  alert(this);
 }
 */
